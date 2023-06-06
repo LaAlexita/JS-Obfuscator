7b436601a6f3ca7891d5ad0847c1a4acc2b6e8c4
Alexita <pikatxu2007@gmail.com>
2023-06-06 16:36:26 +0200

    xd

---
+++ README.md
@@ -0,0 +1,6 @@
+# JSObfuscator
+
+
+Using
+ - https://www.npmjs.com/package/js-confuser
+ - https://www.npmjs.com/package/javascript-obfuscator
--- example.js
+++ example.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 console.log("xxxxxxxxxxxxxxxxxx")
+=======
+console.log("xxxxxxxxxxxxxxxxxx")
+>>>>>>> c7df93ce738105561abdcd00c6c784a6b61d64bf
